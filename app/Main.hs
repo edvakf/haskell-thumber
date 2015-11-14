80844cc38a93b3a60f07e24ae1f1ac605df4540b
Atsushi Takayama <taka.atsushi@gmail.com>
2015-11-14 22:08:43 +0900

    Properly handle upstream response other than 2XX

--- app/Main.hs
+++ app/Main.hs
@@ -34,14 +34,19 @@ thumberApp req respond =
   case parseURI url of
     Nothing -> respond $ responseLBS status400 [] "Invalid URL"
     Just uri -> do
+      (print $ "Requesting: " ++ url)
       result <- httpGet uri
       case result of
         Left _ ->
           respond $ responseLBS status400 [] "Upstream request failed"
         Right rsp ->
-          respond $ responseStream (convStatus rsp) (convHeaders rsp) $ \write flush -> do
-            body <- getResponseBody result
-            write $ BB.fromByteString body
+          case rspCode rsp of
+            (2, _, _) ->
+              respond $ responseStream status200 (convHeaders rsp) $ \write flush -> do
+                body <- getResponseBody result
+                write $ BB.fromByteString body
+            otherwise ->
+              respond $ responseLBS status400 [] "Upstream response is not 2XX"
 
 getPort :: IO Port
 getPort = getEnvironment >>= return . port
@@ -57,12 +62,6 @@ urlFromPath req = (++) "http://" $ intercalate "/" $ map T.unpack $ pathInfo req
 -- use defaultGETRequest_ instead of getRequest because of https://github.com/haskell/HTTP/issues/1
 httpGet :: HStream ty => URI -> IO (Result (Response ty))
 httpGet uri = simpleHTTP (defaultGETRequest_ uri)
-
--- convert upstream response to Wai's response status
-convStatus :: HStream ty => Response ty -> Status
-convStatus rsp =
-  let (x,y,z) = rspCode rsp; reason = rspReason rsp in
-  mkStatus (100*x+10*y+z) (C.pack reason)
 
 -- convert upstream response to Wai's response headers
 convHeaders :: HStream ty => Response ty -> ResponseHeaders
