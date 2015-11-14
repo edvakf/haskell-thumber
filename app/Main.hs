89b4f703ca79988aa22858501744872fc02b6faa
Atsushi Takayama <taka.atsushi@gmail.com>
2015-11-14 18:55:52 +0900

    Proxy jpeg image!

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Network.Wai (responseLBS, Application, pathInfo)
-import Network.HTTP.Types (status200)
+import Network.Wai (responseLBS, responseStream, StreamingBody, Application, pathInfo)
+import Network.HTTP.Types (status200, status400)
+import Network.HTTP (simpleHTTP, getResponseBody, defaultGETRequest_)
+import Network.URI (URI, parseURI)
+import Network.Wai (Request, Response)
 import Network.Wai.Handler.Warp (run, Port)
 import System.Environment (getEnvironment)
 import Data.List (lookup, intercalate)
 import Data.Maybe
 
 import qualified Data.Text as T
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString as B
+import qualified Blaze.ByteString.Builder.ByteString as BB
 
 main :: IO ()
 main = do
@@ -20,7 +23,7 @@ main = do
   run port helloApp
 
 helloApp :: Application
-helloApp req respond = respond $ responseLBS status200 [] (C.pack (urlFromPath req))
+helloApp req respond = respond $ handleRequest req
 
 getPort :: IO Port
 getPort = getEnvironment >>= return . port
@@ -30,4 +33,25 @@ getPort = getEnvironment >>= return . port
 defaultPort :: Port
 defaultPort = 3000
 
-urlFromPath req = ("http://" :: String) ++ (intercalate "/" (map T.unpack (pathInfo req)))
+urlFromPath :: Request -> String
+urlFromPath req = (++) "http://" $ intercalate "/" $ map T.unpack $ pathInfo req
+
+handleRequest :: Request -> Response
+handleRequest req =
+  let url = urlFromPath req in
+  case parseURI url of
+    Nothing -> responseLBS status400 [] "Invalid URL"
+    Just uri -> responseStream status200 [("Content-Type", "image/jpeg")] (lazyByteStringToStream (httpGetLazyByteString uri))
+
+httpGetLazyByteString :: URI -> IO B.ByteString
+httpGetLazyByteString uri = do
+  -- use defaultGETRequest_ instead of getRequest because of https://github.com/haskell/HTTP/issues/1
+  rsp <- simpleHTTP (defaultGETRequest_ uri)
+  body <- getResponseBody rsp
+  return body
+
+lazyByteStringToStream :: IO B.ByteString -> StreamingBody
+lazyByteStringToStream iobs = \write flush -> do
+  bs <- iobs
+  write $ BB.fromByteString bs
+  flush
