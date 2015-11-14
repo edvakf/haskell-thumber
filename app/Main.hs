939d18c5c03bbba0a8ce51cac23a3fd032b25c09
Atsushi Takayama <taka.atsushi@gmail.com>
2015-11-14 21:38:47 +0900

    Handle response other than jpeg and return content-length properly

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Network.Wai (responseLBS, responseStream, StreamingBody, Application, pathInfo)
+import Network.TCP (HStream)
+import Network.Stream (Result)
+import Network.HTTP (Response, simpleHTTP, getResponseBody, defaultGETRequest_, rspCode, rspReason)
 import Network.HTTP.Types (status200, status400)
-import Network.HTTP (simpleHTTP, getResponseBody, defaultGETRequest_)
+import Network.HTTP.Types.Status (Status, mkStatus)
+import Network.HTTP.Types.Header (ResponseHeaders)
+import Network.HTTP.Headers (findHeader, HeaderName(HdrContentLength, HdrContentType))
 import Network.URI (URI, parseURI)
-import Network.Wai (Request, Response)
+import Network.Wai (Request, responseLBS, responseStream, StreamingBody, Application, pathInfo)
 import Network.Wai.Handler.Warp (run, Port)
 import System.Environment (getEnvironment)
 import Data.List (lookup, intercalate)
 import Data.Maybe
+import Data.Either
 
 import qualified Data.Text as T
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 import qualified Blaze.ByteString.Builder.ByteString as BB
 
 main :: IO ()
@@ -20,10 +26,22 @@ main = do
   port <- getPort
   putStr "start Server: http://localhost:"
   print port
-  run port helloApp
+  run port thumberApp
 
-helloApp :: Application
-helloApp req respond = respond $ handleRequest req
+thumberApp :: Application
+thumberApp req respond =
+  let url = urlFromPath req in
+  case parseURI url of
+    Nothing -> respond $ responseLBS status400 [] "Invalid URL"
+    Just uri -> do
+      result <- httpGet uri
+      case result of
+        Left _ ->
+          respond $ responseLBS status400 [] "Upstream request failed"
+        Right rsp ->
+          respond $ responseStream (convStatus rsp) (convHeaders rsp) $ \write flush -> do
+            body <- getResponseBody result
+            write $ BB.fromByteString body
 
 getPort :: IO Port
 getPort = getEnvironment >>= return . port
@@ -36,22 +54,19 @@ defaultPort = 3000
 urlFromPath :: Request -> String
 urlFromPath req = (++) "http://" $ intercalate "/" $ map T.unpack $ pathInfo req
 
-handleRequest :: Request -> Response
-handleRequest req =
-  let url = urlFromPath req in
-  case parseURI url of
-    Nothing -> responseLBS status400 [] "Invalid URL"
-    Just uri -> responseStream status200 [("Content-Type", "image/jpeg")] (lazyByteStringToStream (httpGetLazyByteString uri))
+-- use defaultGETRequest_ instead of getRequest because of https://github.com/haskell/HTTP/issues/1
+httpGet :: HStream ty => URI -> IO (Result (Response ty))
+httpGet uri = simpleHTTP (defaultGETRequest_ uri)
 
-httpGetLazyByteString :: URI -> IO B.ByteString
-httpGetLazyByteString uri = do
-  -- use defaultGETRequest_ instead of getRequest because of https://github.com/haskell/HTTP/issues/1
-  rsp <- simpleHTTP (defaultGETRequest_ uri)
-  body <- getResponseBody rsp
-  return body
+-- convert upstream response to Wai's response status
+convStatus :: HStream ty => Response ty -> Status
+convStatus rsp =
+  let (x,y,z) = rspCode rsp; reason = rspReason rsp in
+  mkStatus (100*x+10*y+z) (C.pack reason)
 
-lazyByteStringToStream :: IO B.ByteString -> StreamingBody
-lazyByteStringToStream iobs = \write flush -> do
-  bs <- iobs
-  write $ BB.fromByteString bs
-  flush
+-- convert upstream response to Wai's response headers
+convHeaders :: HStream ty => Response ty -> ResponseHeaders
+convHeaders rsp =
+  let contentType = fromMaybe "application/octet-stream" (findHeader HdrContentType rsp) in
+  let contentLength = fromMaybe "0" (findHeader HdrContentLength rsp) in
+  [("Content-Type", C.pack contentType), ("Content-Length", C.pack contentLength)]
